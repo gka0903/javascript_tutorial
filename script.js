2be0844054a9f2a6aad01092ed7349575a4ebb28
Haam HyoungBum <59007130+gka0903@users.noreply.github.com>
2022-07-23 08:03:18 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const calculator = {
+	add: function(firstNumber, secondNuber){
+		return firstNumber + secondNuber;
+	},
+	minus: function(firstNumber, secondNuber){
+		console.log(firstNumber - secondNuber);
+	},
+	divide: function(firstNumber, secondNuber){
+		console.log(firstNumber / secondNuber);
+	},
+	multiple: function(firstNumber, secondNuber){
+		console.log(firstNumber * secondNuber);
+	},
+	remainder: function(firstNumber, secondNuber){
+		console.log(firstNumber % secondNuber);
+	},
+}
+
+console.log(calculator.add(4, 6)) ;
